ffff692f94fd2fffea8a682acc050801a9c5f049
vibhanshu <vibhanshu@springboardinvestments.com>
2015-03-21 21:25:52 +0530

    Up and running

--- app/views.py
+++ app/views.py
@@ -1,10 +1,7 @@
 from flask import render_template
 from app import app
 
-
 @app.route('/')
-def base():
-    with open('data.json') as json_data:
-    d = json.loads(json_data)
-    print d
-    return render_template('base.html')
+def hello():
+	return 'my Hello World!'
+
